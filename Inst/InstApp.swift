2bd860a54c231c7b79facc25fc9c549b6ddc8fa4
koko6a <rus.baranovskiy@gmail.com>
2024-01-16 23:48:38 +0300

    git init

---
+++ Inst/InstApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstApp.swift
+//  Inst
+//
+//  Created by Кащенко on 16.01.24.
+//
+
+import SwiftUI
+
+@main
+struct InstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
